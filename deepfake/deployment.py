3eba9c143100c9c8b3306f504cad42ba28e70987
saksh.kr03@gmail.com <sakshamkumarprasad03@gmail.com>
2024-08-13 01:46:24 +0530

    set secret

--- deepfake/deployment.py
+++ deepfake/deployment.py
@@ -2,6 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
+SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = os.environ['WEBSITE_HOSTNAME']
 CSRF_TRUSTED_ORIGINS = ['HTTPS://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
