f9fed9c7946cc11bb1a4c0b590a66dac1318ad85
saksh.kr03@gmail.com <sakshamkumarprasad03@gmail.com>
2024-08-15 21:16:45 +0530

    Development Rollback

--- deepfake/settings.py
+++ deepfake/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "32423grgt4234"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 # Application definition
 
